eb68c3adc7bc0215d01acf5e64858fc09a436d28
Raphael Hanneken <raphaelhanneken@me.com>
2021-01-11 09:31:33 +0100

    add datepicker to select countdown date

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
     import Countdown from './Countdown.svelte';
+    import Datepicker from 'svelte-calendar';
+
+    export let selectedDate: Date = new Date(2021, 0, 27, 0, 0, 0, 0);
 </script>
 
-<main>
-    <Countdown countdownDate={new Date(2021, 0, 27, 0, 0, 0, 0)} />
-</main>
+<countdown>
+    <Datepicker start={new Date()} bind:selected={selectedDate} />
+    <Countdown countdownDate={selectedDate} />
+</countdown>
+
+<style>
+    countdown {
+        padding: 1vw 5vw;
+    }
+</style>
