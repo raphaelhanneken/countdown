c35c155774d02ceb21c85fd4e5b0bb6f7a1a24dd
Raphael Hanneken <raphaelhanneken@me.com>
2021-01-11 08:38:27 +0100

    clean up App component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,30 +1,7 @@
 <script lang="ts">
     import Countdown from './Countdown.svelte';
-    export let name: string;
 </script>
 
 <main>
     <Countdown countdownDate={new Date(2021, 0, 27, 0, 0, 0, 0)} />
 </main>
-
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
-</style>
